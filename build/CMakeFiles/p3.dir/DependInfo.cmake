1775062e69eb16d960af3f80baf78defcaa831bb
David James <dpjames@127x13.csc.calpoly.edu>
2019-05-09 13:23:13 -0700

    fixed dependencies

--- build/CMakeFiles/p3.dir/DependInfo.cmake
+++ build/CMakeFiles/p3.dir/DependInfo.cmake
@@ -1,45 +1,36 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/david/school/s19/csc471/demRender/ext/glad/src/glad.c" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/ext/glad/src/glad.c.o"
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/dpjames/demRender/ext/glad/src/glad.c" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/ext/glad/src/glad.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../ext"
-  "../ext/glad/include"
-  "/home/david/school/s19/csc471/deps/glfw-3.2.1/include"
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dpjames/demRender/ext/tiny_obj_loader/tiny_obj_loader.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "/home/dpjames/demRender/src/GLSL.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/GLSL.cpp.o"
+  "/home/dpjames/demRender/src/MatrixStack.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/MatrixStack.cpp.o"
+  "/home/dpjames/demRender/src/Program.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/Program.cpp.o"
+  "/home/dpjames/demRender/src/Shape.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/Shape.cpp.o"
+  "/home/dpjames/demRender/src/Texture.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/Texture.cpp.o"
+  "/home/dpjames/demRender/src/WindowManager.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/WindowManager.cpp.o"
+  "/home/dpjames/demRender/src/main.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/main.cpp.o"
+  "/home/dpjames/demRender/src/objectDefs.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/objectDefs.cpp.o"
+  "/home/dpjames/demRender/src/tifftest.cpp" "/home/dpjames/demRender/build/CMakeFiles/p3.dir/src/tifftest.cpp.o"
   )
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/david/school/s19/csc471/demRender/ext/tiny_obj_loader/tiny_obj_loader.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/GLSL.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/GLSL.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/MatrixStack.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/MatrixStack.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/Program.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/Program.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/Shape.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/Shape.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/Texture.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/Texture.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/WindowManager.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/WindowManager.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/main.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/main.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/objectDefs.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/objectDefs.cpp.o"
-  "/home/david/school/s19/csc471/demRender/src/tifftest.cpp" "/home/david/school/s19/csc471/demRender/build/CMakeFiles/p3.dir/src/tifftest.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../ext"
-  "../ext/glad/include"
-  "/home/david/school/s19/csc471/deps/glfw-3.2.1/include"
-  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/david/school/s19/csc471/deps/glfw-3.2.1/debug/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../ext"
+  "../ext/glad/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
