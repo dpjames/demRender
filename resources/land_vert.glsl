e92c3801a13218bb5a1c81a07d0cf437bc75a7cb
dpjames <david.patrick.james97@gmail.com>
2019-06-10 01:14:43 -0700

    instanceRendering

--- resources/land_vert.glsl
+++ resources/land_vert.glsl
@@ -2,17 +2,21 @@
 layout(location = 0) in vec4 vertPos;
 layout(location = 1) in vec3 vertNor;
 layout(location = 2) in vec2 vertTex;
+layout(location = 3) in vec4 m1;
+layout(location = 4) in vec4 m2;
+layout(location = 5) in vec4 m3;
+layout(location = 6) in vec4 m4;
 uniform mat4 P;
 uniform mat4 V;
-uniform mat4 M;
 out vec2 vTexCoord;
 out vec3 fragNor;
 out vec3 vpos;
 
 void main()
 {
-	gl_Position = P * V * M * vertPos;
-	fragNor = (V * M * vec4(vertNor, 0.0)).xyz;
-   vpos = (V * M * vertPos).xyz;
+   //mat4 M = mat4(m1,m2,m3,m4);
+	gl_Position = P * V * vertPos;
+	fragNor = (V * vec4(vertNor, 0.0)).xyz;
+   vpos = (V * vertPos).xyz;
    vTexCoord = vertTex;
 }
